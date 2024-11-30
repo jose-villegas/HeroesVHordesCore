fe0daa8f916549d2ff045d71465069f8facd9f25
Jose Villegas <villegasjose.gg@gmail.com>
2024-11-30 16:30:37 +0100

    Using UniRX back instead of R3, seems to have installation issues with Unity 6. Movement working.

--- Assets/Source/Player/PlayerPresenter.cs
+++ Assets/Source/Player/PlayerPresenter.cs
@@ -1,5 +1,5 @@
-﻿using R3;
-using UI;
+﻿using UI;
+using UniRx;
 using UnityEngine;
 using Zenject;
 
@@ -21,16 +21,17 @@ namespace Player
         public void Start()
         {
             // listen to click hold for movement
-            var tapStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0));
+            var tapStream = Observable.EveryUpdate().Where(_ => Input.GetMouseButtonDown(0) || Input.GetMouseButton(0));
 
             tapStream.Subscribe(OnUserTapAndHold);
         }
 
-        private void OnUserTapAndHold(Unit unit)
+        private void OnUserTapAndHold(long unit)
         {
             var speed = playerModel.MoveSpeed;
-            var movement = (Vector3.right + Vector3.forward) * speed;
-           playerView?.Move(movement);
+            var inputDirection = _virtualJoystick.InputDirection;
+            var movement = (Vector3.right * inputDirection.x + Vector3.forward * inputDirection.z) * speed;
+           playerView?.Move(movement * Time.deltaTime);
         }
     }
 }
